919c7b865f30ea81fc5141ab66a247560b688775
Duc Nguyen <baduc.nguyen1992@gmail.com>
2019-03-04 22:38:23 +0700

    add POJOs. Closes #2

---
+++ src/main/java/com/bidi/springguru/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.bidi.springguru.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/bidi/springguru/sfgpetclinic/model/PetType.java
@@ -0,0 +1,5 @@
+package com.bidi.springguru.sfgpetclinic.model;
+
+public class PetType {
+    private String name;
+}
