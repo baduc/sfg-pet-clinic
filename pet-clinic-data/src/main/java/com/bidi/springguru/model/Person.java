787c09ea9072e33386dad960d8b90ad3d7c499c2
Duc Nguyen <baduc.nguyen1992@gmail.com>
2019-03-10 22:05:34 +0700

    adding base entity, close #10

--- pet-clinic-data/src/main/java/com/bidi/springguru/model/Person.java
+++ pet-clinic-data/src/main/java/com/bidi/springguru/model/Person.java
@@ -1,6 +1,6 @@
 package com.bidi.springguru.model;
 
-public class Person {
+public class Person extends BaseEntity{
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/com/bidi/springguru/model/PetType.java
+++ pet-clinic-data/src/main/java/com/bidi/springguru/model/PetType.java
@@ -1,5 +1,5 @@
 package com.bidi.springguru.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
     private String name;
 }
