d310c7f3acdcfea8aafac2cdb223b4ccec600dbb
Duc Nguyen <baduc.nguyen1992@gmail.com>
2019-03-12 22:42:02 +0700

    add Spring config, cloe #22

--- pet-clinic-web/src/main/java/com/bidi/springguru/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/bidi/springguru/sfgpetclinic/bootstrap/DataLoader.java
@@ -4,8 +4,6 @@ import com.bidi.springguru.model.Owner;
 import com.bidi.springguru.model.Vet;
 import com.bidi.springguru.services.OwnerService;
 import com.bidi.springguru.services.VetService;
-import com.bidi.springguru.services.map.OwnerServiceMap;
-import com.bidi.springguru.services.map.VetServiceMap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -15,9 +13,10 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
 
-    public DataLoader() {
-        ownerService = new OwnerServiceMap();
-        vetService = new VetServiceMap();
+    public DataLoader(OwnerService ownerService, VetService vetService) {
+        this.ownerService = ownerService;
+        this.vetService = vetService;
+
     }
 
     @Override
